7aa1f559885a78336e3bdcb6aac233de8de340cd
박환용 <phyu7776@gmail.com>
2024-03-03 16:41:13 +0900

    로그인 기능 구현 (하드코딩)

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,31 +1,59 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <LoginComponent/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginComponent/>}></Route>
+                    <Route path='/login' element={<LoginComponent/>}></Route>
+                    <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                </Routes>
+            </BrowserRouter>            
         </div>
     )
 }
 
 function LoginComponent() {
 
-    const [username, setUsername] = useState('test')
+    const [username, setUsername] = useState('test');
 
-    const [password, setPassword] = useState()
+    const [password, setPassword] = useState();
+
+    const [showSuccessMessage, setshowSuccessMessage] = useState(false);
+
+    const [showErrorMessage, setshowErrorMessage] = useState(false);
+
+    const navigate = useNavigate();
 
     function handleUsernameChange(event) {
-        setUsername(event.target.value)
+        setUsername(event.target.value);
     }
 
     function handlePasswordChange(event) {
-        setPassword(event.target.value)
+        setPassword(event.target.value);
     }
 
+    function handleSubmit() {
+        if (username === 'test' && password === 'dummy') {
+            console.log('Success');
+            setshowSuccessMessage(true);
+            setshowErrorMessage(false);
+            navigate('/welcome');
+        } else {
+            console.log('Failed');
+            setshowErrorMessage(true);
+            setshowSuccessMessage(false) ;
+        }
+    } 
 
     return (
         <div className="Login">
+            {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
+            {showErrorMessage && <div className='errorMessage'>Authenticated Failed. Please check your credentials.</div>}
+
             <div className="LoginForm">
                 <label>User Name:</label>
                 <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
@@ -35,7 +63,7 @@ function LoginComponent() {
                 <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
             </div>
             <div>
-                <button type="button" name="login">
+                <button type="button" name="login" onClick={handleSubmit}>
                     login
                 </button>
             </div>

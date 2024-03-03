4775a16ad976e896e294d9287e0e131205aebb6a
박환용 <phyu7776@gmail.com>
2024-03-03 17:27:41 +0900

    todo 프론트 완료

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, useParams, Link } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
+            <HeaderComponent/>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<LoginComponent/>}></Route>
-                    <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
-                    <Route path='*' element={<ErrorComponent/>}></Route>   
+                    <Route path='/' element={<LoginComponent/>}/>
+                    <Route path='/login' element={<LoginComponent/>}/>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}/>
+                    <Route path='/todos' element={<ListTodosComponent/>}/>
+                    <Route path='/logout' element={<LogoutComponent/>}/>
+                    
+                    <Route path='*' element={<ErrorComponent/>}/> 
                 </Routes>
-            </BrowserRouter>            
+            </BrowserRouter>
+            <FooterComponent/>
         </div>
     )
 }
@@ -83,7 +88,7 @@ function WelcomeComponent() {
         <div className="Welcome">
             <h1>Welcome {username}</h1>
             <div>
-                Welcome Compoenent
+                Manage Your todos. <Link to ='/todos'>Go here</Link>
             </div>
         </div>
     )
@@ -99,3 +104,71 @@ function ErrorComponent() {
         </div>
     )
 }
+
+function ListTodosComponent() {
+
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay());
+
+    const todos =[
+        {id: 1, description: 'Lean AWS',            done: false, targetDate: targetDate},
+        {id: 2, description: 'Lean Full Stack Dev', done: false, targetDate: targetDate},
+        {id: 3, description: 'Lean DevOps',         done: false, targetDate: targetDate}
+    ];
+
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things You Want To Do!</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>id</td>
+                            <td>description</td>
+                            <td>done</td>
+                            <td>targetDate</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {todos.map(todo => (
+                            <tr key={todo.id}>
+                                <td>{todo.id}</td>
+                                <td>{todo.description}</td>
+                                <td>{todo.done.toString()}</td>
+                                <td>{todo.targetDate.toDateString()}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <div className="header">
+            Header <hr/>
+        </div>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <div className="footer">
+            <hr/> Footer
+        </div>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our App. Come back soon!
+            </div>
+        </div>
+    )
+}
+

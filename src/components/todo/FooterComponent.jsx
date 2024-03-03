e41423ae694b92ad114ba4ab840298161ccafcfd
박환용 <phyu7776@gmail.com>
2024-03-03 18:16:29 +0900

    Context를 이용하여 각 컴포넌트에 값 공유

--- src/components/todo/FooterComponent.jsx
+++ src/components/todo/FooterComponent.jsx
@@ -1,4 +1,10 @@
+import { useContext } from 'react';
+import { AuthContext } from './security/AuthContext';
+
 export default function FooterComponent() {
+        
+    const authContext = useContext(AuthContext);
+
     return (
         <footer className="footer">
             <div className='container'>

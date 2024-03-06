c64d0c87896519871e073230cfc39730cd84192f
박환용 <phyu7776@gmail.com>
2024-03-06 23:26:18 +0900

    axios 추가

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,26 @@
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function WelcomeComponent() {
 
     const {username} = useParams();
+
+    function callHelloWorldRestAPI() {
+        console.log('called')
+
+        axios.get('http://localhost:8081/hello-world')
+            .then((responese) => sccessfulResponse(responese))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log('cleanup'));
+    }
+
+    function sccessfulResponse(responese) {
+        console.log(responese)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="Welcome">
@@ -10,6 +28,7 @@ export default function WelcomeComponent() {
             <div>
                 Manage Your todos. <Link to ='/todos'>Go here</Link>
             </div>
+            <button className='btn btn-success m-5' onClick={callHelloWorldRestAPI}>Call Hello World REST API</button>
         </div>
     )
 }
